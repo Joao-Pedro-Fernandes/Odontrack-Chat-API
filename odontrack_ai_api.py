d2e4c164be5c0ca0d6efa33bbfd8bbc76369973b
Joao-Pedro-Fernandes <113483050+Joao-Pedro-Fernandes@users.noreply.github.com>
2025-04-24 21:39:18 -0300

    configurando cors

--- odontrack_ai_api.py
+++ odontrack_ai_api.py
@@ -2,8 +2,19 @@ from groq import Groq
 from fastapi import FastAPI, Request
 from fastapi.responses import StreamingResponse
 from sse_starlette.sse import EventSourceResponse
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Configurar o CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 client = Groq(api_key="")
 
